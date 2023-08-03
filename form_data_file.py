722d44ceae1a6709c4b86a0df6e539bea37a8997
praveen11001 <139953845+praveen11001@users.noreply.github.com>
2023-08-03 16:09:57 +0530

    Update form_data_file.py
    s

--- form_data_file.py
+++ form_data_file.py
@@ -6,20 +6,23 @@ st.header('Responses')
 st.text('We take responses from our customers to reflect on it & provide a quality expertise dining to them')
 
 # Read the data from the URL into a DataFrame
-url = "https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt"
+# url = "https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt"
+
+url = 'https://docs.google.com/spreadsheets/d/1QtWmX5YQD5aswwOYOYerlwdWNwZcmFkxWTr_zAbV9Rk/edit?usp=sharing'
+
 my_response_list = pd.read_csv(url)
 
 
 df = pd.DataFrame(my_response_list)
 
 # ---------------
-csv_link = 'https://docs.google.com/spreadsheets/d/1QtWmX5YQD5aswwOYOYerlwdWNwZcmFkxWTr_zAbV9Rk/edit?usp=sharing'
+# csv_link = 'https://docs.google.com/spreadsheets/d/1QtWmX5YQD5aswwOYOYerlwdWNwZcmFkxWTr_zAbV9Rk/edit?usp=sharing'
 
-# Read the CSV data into a pandas DataFrame
-new_data = pd.read_csv(csv_link)
+# # Read the CSV data into a pandas DataFrame
+# new_data = pd.read_csv(csv_link)
 
-# Now you can work with the 'df' DataFrame containing your Google Sheets data
-pd.dataframe(new_data)
+# # Now you can work with the 'df' DataFrame containing your Google Sheets data
+# pd.dataframe(new_data)
 # ---------------
 
 # Function to apply color to the "Age" column based on a condition
