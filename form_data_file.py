df242e22c29f3438d98a724d9bdd232bac97e5bb
praveen11001 <139953845+praveen11001@users.noreply.github.com>
2023-08-03 15:37:34 +0530

    Update form_data_file.py
    d

--- form_data_file.py
+++ form_data_file.py
@@ -1,10 +1,18 @@
-import streamlit
+import streamlit as st
+import pandas as pd
 
-streamlit.title("My Google Form Sheet Responses")
-  
-streamlit.header('Responses')
-streamlit.text('We take responses from our customers to reflect on it & provide a quality expertice dining to them')
+st.title("My Google Form Sheet Responses")
+st.header('Responses')
+st.text('We take responses from our customers to reflect on it & provide a quality expertise dining to them')
 
-import pandas
-my_response_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_response_list)
+# Read the data from the URL into a DataFrame
+url = "https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt"
+my_response_list = pd.read_csv(url)
+
+# Function to apply color coding to calories column
+def color_calories(val):
+    color = 'red' if val > 100 else 'black'
+    return f'color: {color}'
+
+# Apply the color coding function to the DataFrame and display it
+st.dataframe(my_response_list.style.applymap(color_calories, subset=['calories']))
